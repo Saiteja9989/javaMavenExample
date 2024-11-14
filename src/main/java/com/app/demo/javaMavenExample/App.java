91b046591b28c69a89f43a4f3dd8c9eb254d68bb
saiteja756 <kasojusaiteja1001@gmail.com>
2024-11-14 12:19:36 +0530

    sample Maven java

---
+++ src/main/java/com/app/demo/javaMavenExample/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.javaMavenExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "SAITEJA- 22BD1A054V" );
+    }
+}
